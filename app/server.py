1c815de64c0008ce8fa9820000404e3e223a5f7a
Nelsonlin0321 <nelsonlin0321@outlook.com>
2023-12-24 16:37:53 +0800

    Add healthcheck

--- app/server.py
+++ app/server.py
@@ -1,12 +1,27 @@
 import os
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 import boto3
 import botocore
 import dotenv
 import uvicorn
+from datetime import datetime
+
 dotenv.load_dotenv()
 
 app = FastAPI()
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
@@ -17,7 +32,9 @@ S3_PREFIX = os.getenv("S3_PREFIX")
 
 s3 = boto3.client('s3',aws_access_key_id=AWS_ACCESS_KEY_ID,aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
-@app.post("/api/upload") 
+PREFIX = "/api"
+
+@app.post("{PREFIX}/upload") 
 async def upload_file(file: UploadFile = File(...)): 
     try:
         key = os.path.join(S3_PREFIX,file.filename)
@@ -25,6 +42,16 @@ async def upload_file(file: UploadFile = File(...)):
         return {"message": "File uploaded successfully","bucket":BUCKET_NAME,"key":key} 
     except botocore.exceptions.ClientError as e:
         return {"message": "File upload failed", "error": str(e)}
-    
+
+DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+start_time = datetime.utcnow()
+start_time = start_time.strftime(DATE_FORMAT)
+
+@app.get(f"{PREFIX}/health-check")
+async def health_check():
+    response = f"The server is up since {start_time}"
+    return {"message": response, "start_uct_time": start_time}
+
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000) 
